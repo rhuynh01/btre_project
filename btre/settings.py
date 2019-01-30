8380b6d9091d58f05549c73cc4cc2cbd8e21c996
Rodney Huynh <rhuynh2005@gmail.com>
2019-01-29 23:01:12 -0600

    Removed email from settings

--- btre/settings.py
+++ btre/settings.py
@@ -147,13 +147,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rhuynh2005@gmail.com'
-EMAIL_HOST_PASSWORD = '4Snooker1'
-EMAIL_USE_TLS=True
-
 try:
     from .local_settings import *
 except ImportError:
